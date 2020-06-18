9efbd39ff9a0e1bbf18b822cd0c8661559d229f8
Ryan Sise <kerneltux0@gmail.com>
2020-06-18 10:41:37 -0600

    updated functional_tests.py altered assertion to match project title added user path through site closing browser before printing "test passed"

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,43 @@
 from selenium import webdriver
 
+# user goes to web-site
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# user sees "Solar Panel Calculator" in the title
+assert 'Solar Panel Calculator' in browser.title
 
+# user sees a welcome message describing what the site accomplishes
+
+# user clicks on button labeled "start here"
+
+# user is taken to /location
+
+# user sees message about location data
+
+# user sees a page with one text field & submit button
+
+# field accepts a zip/postal code
+
+# if not valid, user sees an error message
+
+# if valid, user sees success message & button labeled "next"
+
+# user clicks "next" and routed to /power
+
+# user sees message asking for kw/h usage
+
+# user sees message about varying power usage
+
+# user sees one number field & submit button
+
+# user enters kw/h usage & clicks "submit"
+
+# user is routed to /results
+
+# user sees ballpark figure disclaimer
+
+# user sees result in m^2 or ft^2
+
+browser.quit()
 print('Test passed')
