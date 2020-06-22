2e3575ab6467d2ffce22d7ce7c42482a8c1b8c72
Ryan Sise <kerneltux0@gmail.com>
2020-06-22 11:08:39 -0600

    updated views.py in home added redirect & get_object_or_404 to import updated home_page to render home.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return render(request, 'home.html')
