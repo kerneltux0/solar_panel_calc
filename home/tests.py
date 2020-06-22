68bd6fb1c4abe80fec10abcec0a5fb2d9acf3ac0
Ryan Sise <kerneltux0@gmail.com>
2020-06-22 10:26:03 -0600

    updated tests.py in home importing resolve from django.urls importing home_page from home views started class to test home page started method to test url

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from home.views import home_page
 
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
