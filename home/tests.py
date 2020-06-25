2cea6620080781f55a5aacaad0e4afb3f18d116e
Ryan Sise <kerneltux0@gmail.com>
2020-06-25 10:05:50 -0600

    updated tests.py in home changed expectation from button to basic link removed expected redirection

--- home/tests.py
+++ home/tests.py
@@ -20,6 +20,5 @@ class HomePageTest(TestCase):
         self.assertIn('<h1>Welcome to the Solar Panel Calculator</h1>', html)
         self.assertIn('<p>Here, you can figure out how many solar panels you would need to go off-grid according to your current power usage.</p><br>', html)
         self.assertIn("<p>This is meant to be a ballpark figure. This uses data from the Nat'l Renewable Energy Laboratory to grab the amount of useable solar energy in your area, and is only one piece of the puzzle. Going off-grid involves many variables that are best answered by a certified installer.</p><br>", html)
-        self.assertIn('<form><submit>Start!</submit></form>', html)
+        self.assertIn("<a href='/location'>Start!</a>", html)
         self.assertTrue(html.endswith('</html>'))
-        self.assertRedirects(redir)
