112679d4f898be1940d6075aec5714c134f7f93b
Ryan Sise <kerneltux0@gmail.com>
2020-06-25 10:09:43 -0600

    updated urls.py importing locateUser from location views added location as a path

--- solarPanelCalc/urls.py
+++ solarPanelCalc/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import home_page
+from location.views import locateUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('location', locateUser)
 ]
