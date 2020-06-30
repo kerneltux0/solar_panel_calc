a2bfaf16f7ec9a535386551f59ecc615702bdde4
Ryan Sise <kerneltux0@gmail.com>
2020-06-30 09:51:29 -0600

    updated tests.py in location created LocationTest class created test for location url started test for location page content

--- location/tests.py
+++ location/tests.py
@@ -5,3 +5,13 @@ from django.http import HttpRequest
 
 # Create your tests here.
 
+class LocationTest(TestCase):
+
+    def test_location_url(self):
+        found = resolve("/location")
+        self.assertEqual(found.func, locateUser)
+
+    def test_location_content(self):
+        request = HttpRequest()
+        response = locateUser(request)
+        html = response.content.decode('utf8')
