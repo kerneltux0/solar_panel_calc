967210c99a9dc05ccf9eca82c6762ee071ecdae5
Ryan Sise <kerneltux0@gmail.com>
2020-07-07 10:21:57 -0600

    updated tests.py added test for location headline added test for location message

--- location/tests.py
+++ location/tests.py
@@ -15,7 +15,9 @@ class LocationTest(TestCase):
         request = HttpRequest()
         response = locateUser(request)
         html = response.content.decode('utf8')
+        self.assertIn('<h1>Location</h1></br>',html)
         # user sees message about location data
+        self.assertIn('<p>Please enter the zip/postal code for the location where you intend to install the solar panels.</p></br>',html)
         # user sees a form with one text field & submit button
         # user sees error message for invalid postal code
         # pulls location data from API
