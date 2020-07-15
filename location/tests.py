ebd0e61d8dcc30a312e89a63ac021be7a3cb3117
Ryan Sise <kerneltux0@gmail.com>
2020-07-15 09:36:27 -0600

    updated tests.py in location removed ref to API in comment for forum test removed test_location_behavior testing for valid zip/postal code better served as functional test testing session data not appropriate at this stage testing for "next" link better served as functional test

--- location/tests.py
+++ location/tests.py
@@ -18,11 +18,5 @@ class LocationTest(TestCase):
         self.assertIn('<h1>Location</h1></br>',html)
         # user sees message about location data
         self.assertIn('<p>Please enter the zip/postal code for the location where you intend to install the solar panels.</p></br>',html)
-        # user sees a form with one text field, submit button, and pulls location data from API
+        # user sees a form with one text field & submit button
         self.assertIn("<form method='GET'>\n    <input type='text' name='postal code' placeholder='Postal Code'>\n    <input type='submit'/>\n  </form>",html)
-
-    def test_location_behavior(self):
-        
-        # returns empty array for invalid zip/postal code
-        # latitude-longitude saved to session
-        # "next" link appears after session data cached
