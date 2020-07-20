88c89ebe35512b45f0b68fb542665020ea83ad0e
Ryan Sise <kerneltux0@gmail.com>
2020-07-20 11:08:16 -0600

    updated views.py in location set 'location' as context sent to template created conditional for querying API based on form submission reading response from API & creating json object from it rendering raw json as context for testing purposes added comment to create conditional based on API response

--- location/views.py
+++ location/views.py
@@ -4,5 +4,20 @@ import json
 
 # Create your views here.
 def locateUser(request):
-    context = {}
-    return render(request, 'location.html', context)
+    location = {}
+    if 'postal_code' in request.GET:
+        postal_code = request.GET['postal_code']
+        api = http.client.HTTPConnection('api.positionstack.com')
+        params = urllib.parse.urlencode({
+            'access_key': 'my key',
+            'query': postal_code,
+            'limit': 1
+        })
+        api.request('GET', '/v1/forward?{}'.format(params))
+        res = api.getresponse()
+        apiData = res.read()
+        info = json.loads(apiData)
+        ## create conditional for invalid zip/postal code
+        location = info['data'][0]
+
+    return render(request, 'location.html', {'location': location})
