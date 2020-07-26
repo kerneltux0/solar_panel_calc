231c24fadb8cc2f587b792062a46c846da71a607
Ryan Sise <kerneltux0@gmail.com>
2020-07-26 13:11:31 -0600

    updated views.py in location unified naming conventions

--- location/views.py
+++ location/views.py
@@ -7,16 +7,16 @@ def locateUser(request):
     location = {}
     if 'postal_code' in request.GET:
         postal_code = request.GET['postal_code']
-        api = http.client.HTTPConnection('api.positionstack.com')
+        api_url = http.client.HTTPConnection('api.positionstack.com')
         params = urllib.parse.urlencode({
             'access_key': 'my key',
             'query': postal_code,
             'limit': 1
         })
-        api.request('GET', '/v1/forward?{}'.format(params))
-        res = api.getresponse()
-        apiData = res.read()
-        info = json.loads(apiData)
+        api_url.request('GET', '/v1/forward?{}'.format(params))
+        api_response = api_url.getresponse()
+        api_data = api_response.read()
+        info = json.loads(api_data)
         ## create conditional for invalid zip/postal code
         location = info['data'][0]
 
